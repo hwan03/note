a909179df907bac1ad4bbf5d644e3933b5ebae2b
hwan03 <nc9385@naver.com>
2024-11-19 19:20:45 +0900

    태그 기능

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:new_flutter/1.dart';
+import 'package:new_flutter/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
